4d0733d8f568a29a16dbebed37db4e4646bed1eb
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-25 21:36:05 -0400

    Condicionais

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/marcoswlrich/classgo/variaveis"
 )
@@ -10,4 +11,19 @@ func main() {
 	estado, texto := variaveis.ConvertTexto(1999)
 	fmt.Println(estado)
 	fmt.Println(texto)
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Linux")
+	} else {
+		fmt.Println("Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Linux")
+	case "darwin":
+		fmt.Println("Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+
+	}
 }
