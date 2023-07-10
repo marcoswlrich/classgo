1980554330ed86377e0f8d0739306751c590c8c2
marcoswlrich <marcosapw.dev@gmail.com>
2023-07-09 21:54:45 -0400

    GoRoutines

---
+++ goroutines/goroutines.go
@@ -0,0 +1,16 @@
+package goroutines
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func NomeLento(nome string, canal1 chan bool) {
+	letras := strings.Split(nome, "")
+	for _, letra := range letras {
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println(letra)
+	}
+	canal1 <- true
+}
